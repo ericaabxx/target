bd1872a14bf843f74eb605c905d6a040a8635207
Erica Abreu <ericaabxx@gmail.com>
2025-09-06 12:56:59 -0300

    Finalizando Home

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,9 @@
 import { View } from "react-native";
 import { HomeHeader } from "@/components/HomeHeader";
+import { Target } from "@/components/Target";
+import { List } from "@/components/List";
+import { Button } from "@/components/Button";
+import { router } from "expo-router";
 
 const summary = {
     total: "R$ 2.680,00",
@@ -7,10 +11,47 @@ const summary = {
     output: { label: 'Saídas', value: '-R$ 883.65' }
 }
 
+const targets = [
+{   
+    id: "1",
+    name: "Apple Watch",
+    current: "R$ 580,00",
+    percentage: "50%",
+    target: "R$ 1.790,00"
+},
+{   
+    id: "2",
+    name: "Comprar uma cadeira ergonômica",
+    current: "R$ 900,00",
+    percentage: "75%",
+    target: "R$ 1.200"
+},
+{   
+    id: "3",
+    name: "Fazer uma viagem para o Rio de janeiro",
+    current: "R$ 1.200",
+    percentage: "75%",
+    target: "R$ 3.000"
+}
+]
+
 export default function Index(){
     return(
         <View style={{ flex:1 }} >
             <HomeHeader data={summary}/>
+        
+            <List 
+            title="Metas"
+            data={targets} 
+            keyExtractor={(item) => item.id}
+            renderItem={({item}) => <Target data={item} onPress={() => router.navigate(`/in_progress/${item.id}`)} />}
+            emptyMessage="Nenhuma meta. Toque em nova meta para criar."
+            containerStyle={{paddingHorizontal: 24}}
+            />
+
+            <View style={{padding: 24, paddingBottom: 32}}>
+            <Button title="Nova meta" onPress={() => router.navigate("/target")}/>
+            </View>
         </View>
     )
 }
---
+++ src/components/Target/index.tsx
@@ -0,0 +1,31 @@
+import { View, Text, TouchableOpacity, TouchableOpacityProps } from "react-native";
+
+import { styles } from "./style";
+import { MaterialIcons } from "@expo/vector-icons"
+
+export type TargetProps = {
+    id?: string
+    name: string
+    percentage: string
+    current: string
+    target: string
+}
+
+type Props = TouchableOpacityProps & {
+    data: TargetProps
+}
+
+export function Target({data, ...rest}: Props) {
+    return(
+        <TouchableOpacity style={styles.container} {...rest}>
+            <View style={styles.content}>
+                <Text style={styles.name} numberOfLines={1}>{data.name}</Text>
+                <Text style={styles.status}>
+                    {data.percentage} • {data.current} de {data.target}
+                </Text>
+
+                <MaterialIcons name="chevron-right" size={20}/>
+            </View>
+        </TouchableOpacity>
+    )
+}
