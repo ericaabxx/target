ecc8c4864843d80fbbd6d502590bd2e550b0e511
Erica Abreu <ericaabxx@gmail.com>
2025-09-10 19:59:37 -0300

    SQLite - Conexão com o banco de dados

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import { Stack } from "expo-router";
 import {
     useFonts,
@@ -7,6 +8,11 @@ import {
 } from "@expo-google-fonts/inter"
 import { colors } from "@/theme/colors";
 
+import { migrate } from "@/database/migrate";
+
+import { SQLiteProvider } from "expo-sqlite";
+import { Loading } from "@/components/Loading";
+
 export default function Layout(){
     const [fontsLoaded] = useFonts({Inter_400Regular, Inter_500Medium, Inter_700Bold})
 
@@ -14,9 +20,20 @@ export default function Layout(){
         return
     }
     
-    return <Stack  screenOptions={
-        {headerShown: false, 
-        contentStyle: {
-        backgroundColor: colors.white},
-}} />
+    return (
+    <Suspense fallback={<Loading />} >
+        <SQLiteProvider databaseName="target.db" onInit={migrate} useSuspense >        
+            <Stack  
+            screenOptions={
+            {headerShown: false, 
+            contentStyle: {
+            backgroundColor: colors.white},
+    
+            }} 
+        />
+        </SQLiteProvider>
+    </Suspense>
+
+)
 }
+
